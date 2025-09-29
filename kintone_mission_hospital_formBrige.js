8090de5d26cdf5ad4f6632b8044cc4bfb202c88d
chika-kkk <pinavt4253@gmail.com>
2025-09-29 18:05:46 +0900

    Update kintone_mission_hospital_formBrige.js

--- kintone_mission_hospital_formBrige.js
+++ kintone_mission_hospital_formBrige.js
@@ -9,34 +9,6 @@ fb.events.initialized = [function(state) {
                 getMessage: () => '西暦形式（YYYY-MM-DD）で入力してください。',
                 validate: value => /^\d{4}-\d{2}-\d{2}$/.test(value)
             },
-            gender_check: {
-                getMessage: () => '性別は「男」か「女」のいずれかを選択してください。',
-                validate: value => value === '男' || value === '女'
-            }
-            // phone_unique は削除
         };
     };
-
-    fb.events.form.created.push(function(state) {
-        state.fields.find(({code}) => code === "furigana")?.validations.push({
-            params: [],
-            rule: 'hiragana_only'
-        });
-
-        state.fields.find(({code}) => code === "birthdate")?.validations.push({
-            params: [],
-            rule: 'valid_date'
-        });
-
-        state.fields.find(({code}) => code === "gender")?.validations.push({
-            params: [],
-            rule: 'gender_check'
-        });
-
-        return state;
-    });
-
-    // 電話番号の変更時イベントも、重複チェックロジックが不要になったため削除
-    
-    return state;
 }];
