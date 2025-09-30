bf05a308ec3120f0821bc5449d3c10257f3060f4
chika-kkk <pinavt4253@gmail.com>
2025-09-30 14:57:57 +0900

    Update kintone_mission1_Formb.js

--- kintone_mission1_Formb.js
+++ kintone_mission1_Formb.js
@@ -1,3 +1,5 @@
+console.log("表示OK");
+  
 (function () {
   'use strict';
 
@@ -5,7 +7,7 @@
     const record = event.record;
 
     // 編集不可にしたいフィールド一覧
-    const readonlyFields = ['氏名', '生年月日', '病名', '性別', '患者コード'];
+    const readonlyFields = ['氏名', '生年月日', '病名', '性別', '患者コード','既往歴','ふりがな'];
 
     readonlyFields.forEach(function (fieldCode) {
       if (record[fieldCode]) {
