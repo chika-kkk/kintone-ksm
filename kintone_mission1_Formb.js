e2ade2085eff49f1b31337b0489c1012ce0d8f87
chika-kkk <pinavt4253@gmail.com>
2025-09-30 15:19:04 +0900

    Update kintone_mission1_Formb.js

--- kintone_mission1_Formb.js
+++ kintone_mission1_Formb.js
@@ -2,7 +2,7 @@ console.log("表示OK");
   
 document.addEventListener('DOMContentLoaded', function () {
   // 編集不可にしたいフィールドのname属性（フィールドコード）を指定
-  const readonlyFields = ['氏名', '生年月日', '病名', '性別', 'ふりがな','性別', '入院日','担当看護師', '既往歴', 'メモ','担当医サイン', '担当医'];
+  const readonlyFields = ['氏名', '生年月日', '病名', '性別', 'ふりがな', '入院日','担当看護師', '既往歴', 'メモ','担当医サイン', '担当医'];
 
   readonlyFields.forEach(function (fieldCode) {
     const input = document.querySelector(`[name="${fieldCode}"]`);
